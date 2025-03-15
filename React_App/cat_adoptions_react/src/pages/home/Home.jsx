073b0063bb7267d5ef7ab9257071d4da474ed5bd
albumaria <albumaria23@gmail.com>
2025-03-15 21:23:46 +0200

    added filter functionality

--- React_App/cat_adoptions_react/src/pages/home/Home.jsx
+++ React_App/cat_adoptions_react/src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Button from "../../components/button/Button";
 import "./Home.css";
 import {Link} from 'react-router-dom';
 import Rectangle from "../../components/rectangle/Rectangle";
-import BannerImage from "../../assets/tole.jpg"
 
 const Home = () => {
     return (
@@ -19,7 +18,7 @@ const Home = () => {
                         <Button content={"To Add Later"} />
                     </Link>
                 </Rectangle>
-                <Rectangle type="right" width="50%" backgroundColor="#FFDD4D" imageSrc={BannerImage} />
+                <Rectangle type="right" width="50%" backgroundColor="#FFDD4D" imageSrc="https://i.pinimg.com/736x/db/28/b2/db28b29fc597f6fd9261fb616999776f.jpg" />
             </div>
         </div>
     )
